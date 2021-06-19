bf23323e92e1a50375d44a88a1f7ccb8074e833d
sdeda1us <steven.maloney@gmail.com>
2021-06-19 18:53:04 -0500

    products appear on page

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../shared/product.service';
 
 @Component({
   selector: 'nga-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
-export class ProductItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProductItemComponent {
+  @Input() product: Product;
 }
